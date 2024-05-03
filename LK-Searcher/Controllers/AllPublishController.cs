d8d9eda7996d8cb62e257fdaaa6a24e4ad178dcc
Danil Baliuk <Danya9997@mail.ru>
2024-05-03 21:56:00 +0300

    Рабочий вариант

--- LK-Searcher/Controllers/AllPublishController.cs
+++ LK-Searcher/Controllers/AllPublishController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LK_Searcher.EntityModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace LK_Searcher.Controllers
@@ -8,9 +9,57 @@ namespace LK_Searcher.Controllers
         MyContext dbContext = new MyContext();
         public IActionResult Index()
         {
-            var publishes = dbContext.Publishes.ToList();
+            
+            var publishes = dbContext.Publishes.Include(a => a.Authors).ToList();
 
             return View(publishes);
+        }
+        public IActionResult AddNewPublish()
+        {
+            var authors = dbContext.Authors.ToList();
+
+            return View(authors);
+        }
+        [HttpPost]
+        public IActionResult AddAuthor(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.Authors.Add(author);
+                dbContext.SaveChanges();
+            }
+            var authors = dbContext.Authors.ToList();
+            return View("AddNewPublish", authors);
+        }
+        public IActionResult AddPublish(Publish publish)
+        {//Authors
+            if (ModelState.IsValid)
+            {
+                List<Author> authors = new List<Author>();
+                var rawValue = ModelState["Authors"].RawValue;
+                if (rawValue is string authorId)
+                {
+                    // Обработка случая, когда у статьи только один автор
+                    int authorIdInt = int.Parse(authorId);
+                    var author = dbContext.Authors.FirstOrDefault(a => a.Id == authorIdInt);
+                    if (author != null)
+                    {
+                        authors.Add(author);
+                    }
+                }
+                else if (rawValue is string[] authorIdStrings)
+                {
+                    authorIdStrings = (string[])ModelState["Authors"].RawValue;
+                    int[] authorIds = authorIdStrings.Select(int.Parse).ToArray();
+                    authors = dbContext.Authors.Where(a => authorIds.Contains(a.Id)).ToList();
+                }
+
+                publish.Authors = authors;
+                dbContext.Publishes.Add(publish);
+                dbContext.SaveChanges();
+            }
+            var publishes = dbContext.Publishes.ToList();
+            return View("Index", publishes);
         }
     }
 }
--- LK-Searcher/Controllers/PersonalAreaController.cs
+++ LK-Searcher/Controllers/PersonalAreaController.cs
@@ -10,43 +10,9 @@ namespace LK_Searcher.Controllers
         {
             return View();
         }
-        public IActionResult ShowAllPublish()
-        {
-            var publishes = dbContext.Publishes.ToList();
-
-            return View(publishes);
-        }
-        public IActionResult AddNewPublish()
-        {
-            var authors = dbContext.Authors.ToList();
-
-            return View(authors);
-        }
-        [HttpPost]
-        public IActionResult AddAuthor(Author author)
-        {
-            if (ModelState.IsValid)
-            {
-                dbContext.Authors.Add(author);
-                dbContext.SaveChanges();
-            }
-            var authors = dbContext.Authors.ToList();
-            return View("AddNewPublish", authors);
-        }
-        public IActionResult AddPublish(Publish publish)
-        {//Authors
-            if (ModelState.IsValid)
-            {
-                string[] authorIdStrings = (string[])ModelState["Authors"].RawValue;
-                int[] authorIds = authorIdStrings.Select(int.Parse).ToArray();
-                var authors = dbContext.Authors.Where(a => authorIds.Contains(a.Id)).ToList();
-                publish.Authors = authors;
-                dbContext.Publishes.Add(publish);
-                dbContext.SaveChanges();
-            }
-            var publishes = dbContext.Publishes.ToList();
-            return View("ShowAllPublish", publishes);
-        }
+    
+        
+        
 
     }
 }
